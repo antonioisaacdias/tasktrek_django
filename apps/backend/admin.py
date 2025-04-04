30bbb2227672a2001c77e47c31eadf351938f17b
Antonio Dias <antonioisaacvd@gmail.com>
2025-04-03 23:02:25 -0300

    feat: Implementa models.

--- apps/backend/admin.py
+++ apps/backend/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Project, Task
+
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color','description', 'comments', 'created_at', 'updated_at', 'user', 'is_active')
+    search_fields = ('name', 'color','description', 'comments')
+    list_filter = ('is_active', 'created_at')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    readonly_fields = ('id', 'user', 'created_at', 'updated_at')
+    list_select_related = ('user',)
+    
+    def save_model(self, request, obj, form, change):
+        # Corrige tanto para criação quanto atualização se necessário
+        if not obj.user_id:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+    
+@admin.register(Task)    
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'comments', 'created_at', 'updated_at', 'expires_at', 'is_completed', 'user', 'project')
+    search_fields = ('name', 'comments')
+    list_filter = ('is_completed',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    list_select_related = ('user', 'project')
+    readonly_fields = ('id', 'user', 'created_at', 'updated_at')
+    list_select_related = ('user',)
+    
+
+    def save_model(self, request, obj, form, change):
+        # Corrige tanto para criação quanto atualização se necessário
+        if not obj.user_id:
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
+
--- apps/backend/models.py
+++ apps/backend/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from uuid import uuid4
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=7, default='#000000')
+    description = models.TextField(null=True, blank=True)
+    comments = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects', editable=False)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Projeto'
+        
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    comments = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField(default=now)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', editable=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks', null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Tarefa'
+
+    def __str__(self):
+        return self.name
